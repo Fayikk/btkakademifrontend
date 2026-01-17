28e7c7e434d3660613be1cba7f5455ff5bb9eb68
Fayikk <fayik.v21@gmail.com>
2026-01-17 16:40:49 +0300

    Refactor ProductDTO interface structure and clean up commented code

--- lib/type.ts
+++ lib/type.ts
@@ -11,3 +11,23 @@ export interface CategoryDTO {
     description:string;
     createBy:string;
 }
+
+//  public string ProductName { get; set; }
+//  public string ProductDescription { get; set; }
+
+//  public string ImageUrl { get; set; }
+//  public int StockAmount { get; set; }
+//  public decimal Price { get; set; }
+//  public Guid CategoryId { get; set; }
+
+
+
+export interface ProductDTO{
+    id:string;
+    productName:string;
+    productDescription:string;
+    imageUrl:string;
+    stockAmount:number;
+    price:number;
+    categoryId:string;
+}
